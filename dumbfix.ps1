1c41549a70ee9bf99233cd037b6aa8daffe1be6a
H3XDaemon <42803553+H3XDaemon@users.noreply.github.com>
2022-09-23 12:31:05 +0800

    Update dumbfix.ps1

--- dumbfix.ps1
+++ dumbfix.ps1
@@ -2,9 +2,9 @@ Move-Item -Path C:\GMT軟體\gs -Destination C:\gs
 Move-Item -Path C:\GMT軟體\programs -Destination C:\programs
 [Environment]::SetEnvironmentVariable(
     "Path",
-    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\gs\gs9.07\bin",
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";C:\gs\gs9.07\bin",
     [EnvironmentVariableTarget]::Machine)
 [Environment]::SetEnvironmentVariable(
     "Path",
-    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\programs\gmt\bin",
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";C:\programs\gmt\bin",
     [EnvironmentVariableTarget]::Machine)
