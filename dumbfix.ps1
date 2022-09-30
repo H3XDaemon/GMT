d9232057b45298f7f706a3bcbcfa3241f2679347
H3XDaemon <42803553+H3XDaemon@users.noreply.github.com>
2022-09-30 12:39:18 +0800

    Update dumbfix.ps1

--- dumbfix.ps1
+++ dumbfix.ps1
@@ -1,3 +1,5 @@
+
+
 Move-Item -Path C:\GMT軟體\gs -Destination C:\gs
 Move-Item -Path C:\GMT軟體\programs -Destination C:\programs
 Invoke-RestMethod -URI "https://raw.githubusercontent.com/H3XDaemon/GMT/main/1027.bat" -OutFile "D:\1027.bat"
@@ -10,3 +12,7 @@ Invoke-RestMethod -URI "https://raw.githubusercontent.com/H3XDaemon/GMT/main/102
     "Path",
     [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";C:\programs\gmt\bin",
     [EnvironmentVariableTarget]::User)
+
+start-process "cmd.exe" "/c D:1027.bat"
+
+
