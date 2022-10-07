b7ee2cd5d658893156cf9f25f86af6cc34e08faa
H3XDaemon <42803553+H3XDaemon@users.noreply.github.com>
2022-10-07 12:34:49 +0800

    Update GMT-env-fix.ps1

--- GMT-env-fix.ps1
+++ GMT-env-fix.ps1
@@ -1,7 +1,3 @@
-Move-Item -Path C:\GMT軟體\gs -Destination C:\gs
-Move-Item -Path C:\GMT軟體\programs -Destination C:\programs
-Invoke-RestMethod -URI "https://raw.githubusercontent.com/H3XDaemon/GMT/main/gmtest.bat" -OutFile "D:\gmtest.bat"
-
 [Environment]::SetEnvironmentVariable(
     "Path",
     [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";C:\gs\gs9.07\bin",
@@ -11,5 +7,8 @@ Invoke-RestMethod -URI "https://raw.githubusercontent.com/H3XDaemon/GMT/main/gmt
     [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";C:\programs\gmt\bin",
     [EnvironmentVariableTarget]::User)
 
+Move-Item -Path C:\GMT軟體\gs -Destination C:\gs
+Move-Item -Path C:\GMT軟體\programs -Destination C:\programs
+Invoke-RestMethod -URI "https://raw.githubusercontent.com/H3XDaemon/GMT/main/gmtest.bat" -OutFile "D:\gmtest.bat"
 Invoke-RestMethod -URI "https://raw.githubusercontent.com/H3XDaemon/GMT/main/GMT-env-fix.ps1" -OutFile "D:\GMT-env-fix.ps1"
 Start-Process -FilePath "D:\gmtest.bat"
